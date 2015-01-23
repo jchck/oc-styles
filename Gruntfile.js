90c822cee889bcb947491d222d5e75a772462e2a
jaechick <hey@justinchick.com>
2015-01-22 19:18:00 -0600

    typos, whoops!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,59 @@
-moduel.exports = function(grunt){
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
 	grunt.initConfig({
 		less: {
 			dev: {
 				files: {
-					'assets/css/main.css':[
+					'assets/css/main.css': [
 					'assets/less/main.less'
 					]
 				},
 				options: {
-					compress: false
+					compress: false,
+					sourceMap: true,
+					sourceMapFilename: 'assets/css/main.css.map'
 				}
 			},
 			build: {
 				files: {
-					'asset/css/main.min':[
-					'asset/less/main.less'
+					'assets/css/main.min.css': [
+					'assets/less/main.less'
 					]
 				},
 				options: {
 					compress: true
 				}
 			}
+		},
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie 8', 'ie 9', 'android 2.3', 'android 4', 'opera 12']
+			},
+			dev: {
+				options: {
+					map: {
+						prev: 'assets/css/'
+					}
+				},
+				src: 'assets/css/main.css'
+			},
+			build: {
+				src: 'assets/css/main.min.css'
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-less');
-
-	grunt.registerTasks('default', [
+	grunt.registerTask('default', [
 		'dev'
 	]);
-	grunt.registerTasks('dev', [
-		'less:dev'
+	grunt.registerTask('dev', [
+		'less:dev',
+		'autoprefixer:dev'
 	]);
-	grunt.registerTasks('build', [
-		'less:build'
+	grunt.registerTask('build', [
+		'less:build',
+		'autoprefixer:build'
 	]);
 };
